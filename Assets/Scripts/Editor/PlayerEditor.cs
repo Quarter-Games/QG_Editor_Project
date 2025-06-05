5c7c9553c02763f3941485c96b41bcc0f58a4df4
yoavk-sudo <kenkendler123@gmail.com>
2025-06-05 19:47:37 +0300

    help box

--- Assets/Scripts/Editor/PlayerEditor.cs
+++ Assets/Scripts/Editor/PlayerEditor.cs
@@ -11,8 +11,36 @@ public class PlayerEditor : Editor
         GUIStyle coloredLabel = new GUIStyle(EditorStyles.whiteBoldLabel);
         coloredLabel.normal.textColor = player.labelColor;
         GUILayout.Label("This is the player controller script", coloredLabel);
-        base.OnInspectorGUI();
-        GUILayout.Label("test", EditorStyles.foldout);
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Move Speed");
+        Color speedColor = player.moveSpeed < 0 ? Color.red : Color.green;
+        GUIStyle style = new GUIStyle(GUI.skin.textField);
+        style.normal.textColor = speedColor;
+        string input = GUILayout.TextField(player.moveSpeed.ToString(), style, GUILayout.Width(100));
+        if (float.TryParse(input, out float newSpeed))
+        {
+            if (newSpeed != player.moveSpeed)
+            {
+                Undo.RecordObject(player, "Change Move Speed");
+                player.moveSpeed = newSpeed;
+                EditorUtility.SetDirty(player);
+            }
+        }
+        GUILayout.EndHorizontal();
+        switch (player.moveSpeed)
+        {
+            case < 0:
+                EditorGUILayout.HelpBox("Move speed cannot be negative!", MessageType.Error);
+                break;
+            case 0:
+                EditorGUILayout.HelpBox("Move speed is zero, the player won't move!", MessageType.Warning);
+                break;
+            case > 10:
+                EditorGUILayout.HelpBox("Move speed is quite high!", MessageType.Info);
+                break;
+            default:
+                break;
+        }
     }
 
 }
